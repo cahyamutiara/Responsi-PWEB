a7f8f5b45fd21fb9278c98d0659906c47b6d149b
Cahya <101860958+cahyamutiara@users.noreply.github.com>
2022-07-22 00:14:59 +0700

    Update ongkir.php

--- ongkir.php
+++ ongkir.php
@@ -5,7 +5,7 @@ if (!empty($_POST['name']) && !empty($_POST['distance']) && !empty($_POST['addre
 	$address = $_POST['address'];
 	if (is_numeric($_POST['distance'])) {
 		$deliveryPrice = ((int)$_POST['distance'] * 5000);
-		$data = $name . ', ' . $address . ', ' . $distance . ', ' . $deliveryPrice;
+		$data = $name . ', ' . $address . ', ' . $distance . ', ' . $deliveryPrice . "\n";
 		file_put_contents(getcwd() . '/data.txt', $data, FILE_APPEND);
 		echo json_encode(array(
 			'success' => 1,
